2eb5d61f1ad4579aa287c13d0e4b780bc47f69d1
Packi1992 <patrick.gehlich@mail.de>
2023-06-04 17:20:45 +0200

    added more to wave struct

--- src/tdUtil/Wave.cpp
+++ src/tdUtil/Wave.cpp
@@ -4,18 +4,49 @@
 
 #include "Wave.h"
 
-Wave::Wave(int SpawnCount) {
+Wave::Wave(int SpawnCount ) {
     enemies.resize(SpawnCount);
 }
 
 Wave::~Wave() {
-
+    enemies.clear();
+    pendingEvents.clear();
 }
 
 void Wave::addEvent(string Event) {
-
+    // we need to define a function to write and load Spawn Events ...
+    // maybe inside Struct?
+    addEvent(SpawnEvent::readLine(Event));
 }
 
-void Wave::PollEvent(SpawnEvent *event) {
+bool Wave::PollEvent(SpawnEvent &event) {
+    if(pendingEvents.empty())
+        return false;
+    event = pendingEvents.front();
+    pendingEvents.pop_front();
+    return true;
+}
 
+void Wave::Update(const u32 totalMSec) {
+    u32 now = totalMSec-waveStart;
+    for (SpawnEvent event:enemies) {
+        if(event.time > now){
+            pendingEvents.push_back(event);
+        }
+    }
+    for (SpawnEvent event: pendingEvents){
+        for(int i =0 ; i< enemies.size();i++){
+            enemies.erase(std::remove(enemies.begin(), enemies.end(),event),enemies.end());
+        }
+    }
+}
+
+void Wave::addEvent(SpawnEvent Event) {
+    enemies.push_back(Event);
+    std::sort(enemies.begin(),enemies.end());
+}
+
+void Wave::startWave(const u32 totalMSec) {
+    waveStart = totalMSec;
+    hasStarted = true;
 }
--- src/tdUtil/Wave.h
+++ src/tdUtil/Wave.h
@@ -7,6 +7,8 @@
 
 #include "../gamebase.h"
 #include "enemy.h"
+#include <list>
+#include <algorithm>
 
 
 struct SpawnEvent{
@@ -20,6 +22,22 @@ struct SpawnEvent{
     u8 SpawnPoint=0;
     //  value of enemy
     u16 value=1;
+    bool operator< (const SpawnEvent &other) const{
+        return time < other.time;
+    }
+
+    bool operator== (const SpawnEvent &other) const{
+        return (type == other.type)&&
+                (time == other.time)&&
+                (count == other.count)&&
+                (SpawnPoint == other.SpawnPoint)&&
+                (value == other.value);
+    }
+
+    static SpawnEvent readLine(string EventAsString) {
+        cerr << "implement Spawn Event parsing from String";
+        return SpawnEvent{};
+    }
 };
 
 class Wave{
@@ -28,13 +46,26 @@ public:
     // is it possible to sort a vector with specific element attribute?
     // sort with given time
     explicit Wave(int SpawnCount);
+    // add Event takes a String should be easier to load
     void addEvent(string Event);
-    ~Wave();
-    void PollEvent(SpawnEvent *event);
+    // add Event kann also handle an actual spawn event
+    void addEvent(SpawnEvent Event);
+    // simply sets waveStart, it will be used to calculate spawn events
+    void startWave(const u32 totalMSec);
+
+    // will be false, if que is empty
+    bool PollEvent(SpawnEvent &event);
+    void Update(const u32 totalMSec);
     // index showing wave progress
     int index=0;
+    ~Wave();
 private:
-    Vector<SpawnEvent> enemies;
+    Vector<SpawnEvent> enemies;    // i want to implement a min heap ...
+    std::list<SpawnEvent> pendingEvents;
+    u32 waveStart=0;
+    bool hasStarted = false;
+    bool hasEnded = false;
+
 };
 
 
