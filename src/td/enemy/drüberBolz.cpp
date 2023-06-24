fe43536211a799e73f6e8fd62cde47a9adc3bb37
Jesrago <jonas.westhofen13@gmail.com>
2023-06-24 17:32:50 +0200

    Spawning Bosses

--- src/td/enemy/drüberBolz.cpp
+++ src/td/enemy/drüberBolz.cpp
@@ -2,5 +2,7 @@
 
 DrüberBolz::DrüberBolz(FPoint pos, uint16_t health, uint8_t speed, u8 value, EnemyType type, float size, bool stunable)
 	: BossEnemy(pos, health, speed, value, type, size, stunable) {
-
+	_copyable = false;
+	_sanity *= 3;
+	_value *= 3;
 }
